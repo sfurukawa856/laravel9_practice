a302daabf135393114da91abc62a4bd2233fce51
sfurukawa <tennis856@gmail.com>
2022-07-30 01:06:57 +0900

    削除機能追加

--- resources/views/tweet/index.blade.php
+++ resources/views/tweet/index.blade.php
@@ -12,7 +12,10 @@
 	<h1>つぶやきシローアプリ</h1>
 	<div>
 		<p>投稿フォーム</p>
-		<form action="{{ route('tweet.create') }}" method="post">
+		@if(session('feedback.success'))
+		<p style="color:green">{{ 'feedback.success' }}</p>
+		@endif
+		<form action=" {{ route('tweet.create') }}" method="post">
 			@csrf
 			<label for="tweet-content">つぶやき</label>
 			<span>140文字まで</span>
@@ -29,6 +32,11 @@
 			<summary>{{ $tweet->content }}</summary>
 			<div>
 				<a href="{{ route('tweet.update.index',['tweetId'=>$tweet->id]) }}">編集</a>
+				<form action="{{ route('tweet.delete',['tweetId'=>$tweet->id]) }}" method="post">
+					@method('delete')
+					@csrf
+					<button type="submit">削除</button>
+				</form>
 			</div>
 		</details>
 		@endforeach
